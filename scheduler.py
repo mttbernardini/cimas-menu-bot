c72e57199b44bcc49d2919a545bbd10af568ad9f
Matteo Bernardini <mat_teobe@yahoo.it>
2019-03-24 22:29:44 +0100

    test cron (fix)

--- scheduler.py
+++ scheduler.py
@@ -10,7 +10,7 @@ import cimas_bot
 sched = BlockingScheduler()
 
 #@sched.scheduled_job('cron', day_of_week='mon-fri', hour=12)
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=22, minute=32)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=22, minute=32)
 def scheduled_job():
     cimas_bot.main()
 
