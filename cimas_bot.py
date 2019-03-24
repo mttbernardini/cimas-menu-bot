00c2992c7726f12825e0a0f24f119fec63621cb8
Matteo Bernardini <mat_teobe@yahoo.it>
2019-03-24 22:10:05 +0100

    unificato script e altre modifiche

--- cimas_bot.py
+++ cimas_bot.py
@@ -1,42 +1,60 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2019 Matteo Bernardini
 # Copyright (c) 2019 Gianmaria Del Monte
 
-import telepot, locale, menus, calendar
-from datetime import datetime
+import telepot, requests, locale, lxml.html
+from sys import argv
+from datetime import datetime, date
 
-TOKEN='633120718:AAGPAeiYo7xWwAdM9lGeG0GotmGlzJXQ8QQ'
+locale.setlocale(locale.LC_TIME, ("it_IT", "UTF-8"))
+
+MENU_URL = "http://www.cimasristorazione.com/menu-mense/universita-roma-3/"
 CHANNEL_ID = -1001186126012
 
-DEFAULT_MSG = "Il menu di oggi non è disponibile"
 
-def get_today_menu():
-	locale.setlocale(locale.LC_TIME, ("it_IT", "UTF-8"))
-	now = datetime.now().date()
+def get_menus():
+	r = requests.get(MENU_URL)
+	dom = lxml.html.fromstring(r.text)
+	menus = {}
+	for div in dom.cssselect(".page-menu-mense .day-menu"):
+		day = datetime.strptime(div.find_class("date")[0].text, "%d %B %Y").date()
+		day_menu = {}
+		for (h5, ul) in zip(div.cssselect(".tab-content h5"), div.cssselect(".tab-content ul")):
+			title = h5.text.strip()
+			items = list(map(lambda x: x.text.strip(), ul.cssselect("li")))
+			day_menu[title] = items
+		menus[day] = day_menu
+	return menus
+
+def todays_menu_to_md(menus):
+	day = date(2019, 3, 18)
+	menu = menus[day]
 	
-	try:
-		menu = menus.getMenus()[now]
-		msg = "🍴 Il menu di oggi (%s %d %s)\n" % (calendar.day_name[now.weekday()], now.day, calendar.month_name[now.month])
-		msg += "-------------------------------------\n"
+	msg = "🍴 Il menu di oggi _(%s)_\n" % (day.strftime("%A %d %B"))
+	msg += "-------------------------------------\n"
+	
+	for p in menu:
+		msg += "*%s:*\n" % p
+		for pasto in menu[p]:
+			msg += " - %s\n" % pasto
 		
-		for p in menu:
-			msg += "*%s:*\n" % p
-			for pasto in menu[p]:
-				msg += " - %s\n" % pasto
-			
-		msg += "-------------------------------------\n"
-		msg += "🍞 _Il pane è compreso nel pasto._\n"
-		msg += "🥤 _Le bevande sono disponibili nei distributori all'interno della sala e sono libere._\n"
-		msg += "🍊 _In alternativa alla frutta: yogurt, succhi di frutta o dessert._\n"
-		return msg
-		
-	except:
-		return DEFAULT_MSG
+	msg += "-------------------------------------\n"
+	msg += "🍞 _Il pane è compreso nel pasto._\n"
+	msg += "🥤 _Le bevande sono disponibili nei distributori all'interno della sala e sono libere._\n"
+	msg += "🍊 _In alternativa alla frutta: yogurt, succhi di frutta o dessert._\n"
 
-def send_msg(msg):
-	bot = telepot.Bot(TOKEN)
-	bot.sendMessage(CHANNEL_ID, msg, parse_mode= 'Markdown')
+	return msg
 
-if __name__ == '__main__':
-	send_msg(get_today_menu())
+def send_msg(msg, token):
+	bot = telepot.Bot(token)
+	bot.sendMessage(CHANNEL_ID, msg, parse_mode="Markdown")
+
+def main(token=None):
+	menus  = get_menus()
+	msg    = todays_menu_to_md(menus)
+	send_msg(msg, token)
+
+if __name__ == "__main__":
+	main(argv[1])
--- scheduler.py
+++ scheduler.py
@@ -4,13 +4,14 @@
 # Copyright (c) 2019 Matteo Bernardini
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-import menus
+from os import environ
+import cimas_bot
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=12)
 def scheduled_job():
-    menus.getMenus()
+    cimas_bot.main(environ["TG_TOKEN"])
 
 print("scheduler started")
 sched.start()
